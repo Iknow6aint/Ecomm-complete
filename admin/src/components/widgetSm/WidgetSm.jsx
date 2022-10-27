c8f883384534d2b3860ef81f3ecded9192abd154
iknow6aint <saintagbukor@gmail.com>
2022-10-27 18:52:43 -0300

    getting new members

--- admin/src/components/widgetSm/WidgetSm.jsx
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -1,87 +1,45 @@
 import "./widgetSm.css";
 import { Visibility } from "@material-ui/icons";
+import { useState } from "react";
+import { useEffect } from "react";
+import { userRequest } from "../../requests";
 
 export default function WidgetSm() {
-  return (
-    <div className="widgetSm">
-      <span className="widgetSmTitle">New Join Members</span>
-      <ul className="widgetSmList">
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-      </ul>
-    </div>
-  );
+
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        const getUsers = async () => {
+            try {
+                const res = await userRequest.get("/users/?new=true");
+                setUsers(res.data)
+                console.log(res.data);
+            } catch (error) { }
+        }
+        getUsers()
+    }, [])
+    return (
+        <div className="widgetSm">
+            <span className="widgetSmTitle">New Join Members</span>
+            <ul className="widgetSmList">
+                {users.map((user) => (
+                    <li className="widgetSmListItem" key={user.id}>
+                        <img
+                            src={user.img}
+                            alt=""
+                            className="widgetSmImg"
+                        />
+                        <div className="widgetSmUser">
+                            <span className="widgetSmUsername">{user.username}</span>
+                        </div>
+                        <button className="widgetSmButton">
+                            <Visibility className="widgetSmIcon" />
+                            Display
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
--- admin/src/requests.js
+++ admin/src/requests.js
@@ -13,5 +13,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
     baseURL: APIURL,
-    header: { token: `Bearer ${TOKEN}` },
+    headers: { token: `Bearer ${TOKEN}` },
 })
