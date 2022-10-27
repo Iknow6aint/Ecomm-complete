abf76169767a8889eff52ffa330b73794c32f53a
iknow6aint <saintagbukor@gmail.com>
2022-10-27 05:08:59 -0300

    user persist

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -2,10 +2,39 @@ import { configureStore } from "@reduxjs/toolkit"
 import cartRedux from "./cartRedux"
 import userSlice from "./userSlice"
 
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default configureStore({
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+};
+
+
+const persistedReducer = persistReducer(persistConfig, userSlice);
+
+export const store = configureStore({
     reducer: {
         cart: cartRedux,
-        user: userSlice
-    }
+        user: persistedReducer
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
+
+export let persistor = persistStore(store);
