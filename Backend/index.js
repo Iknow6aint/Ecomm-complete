8c87cd51197ce61ad96612d67f443b116d8c2a41
iknow6aint <saintagbukor@gmail.com>
2022-11-14 10:06:38 -0400

    error refactor

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
     next();
 });
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: '*'
 }));
 app.use(express.json());
 app.use("/api/auth", authRoute);
--- Frontend/src/redux/apiCalls.js
+++ Frontend/src/redux/apiCalls.js
@@ -1,5 +1,9 @@
 import { loginFailure, loginStart, loginSuccess, registerStart, registerSuccess, registerFailure } from "./userSlice"
 import { publicRequest, userRequest } from "../requests"
+import axios from "axios";
+
+const APIURL = 'http://localhost:5000/api/auth/register'
+
 
 
 export const login = async (dispatch, user) => {
@@ -12,10 +16,17 @@ export const login = async (dispatch, user) => {
     }
 }
 
-export const register = async (user, dispatch) => {
+export const register = async (dispatch, user) => {
     dispatch(registerStart());
+    console.log(typeof user, JSON.stringify(user));
     try {
-        const res = await publicRequest.post("/auth/register", user)
+        const res = await fetch(APIURL, {
+            method: 'POST',
+            body: JSON.stringify(JSON.parse(user)),
+            headers: {
+                "content-type": "application/json"
+            }
+        })
         dispatch(registerSuccess(res.data))
     } catch (error) {
         dispatch(registerFailure())
